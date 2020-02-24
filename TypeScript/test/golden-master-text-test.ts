3748d74e67f1f03a99562c40450a93be2b9b1fc2
Tom Davies <tom@thenuel.com>
2020-02-24 09:37:00 +0000

    Fixes test script to account for immutability

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -13,15 +13,15 @@ const items = [
     new Item("Conjured Mana Cake", 3, 6)];
 
 
+let updatedItems = items;
 const gildedRose = new GildedRose(items);
 var days: number = 2;
 for (let i = 0; i < days; i++) {
     console.log("-------- day " + i + " --------");
     console.log("name, sellIn, quality");
-    items.forEach(element => {
+    updatedItems.forEach(element => {
         console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
-
     });
     console.log();
-    gildedRose.updateQuality();
+    updatedItems = gildedRose.updateQuality();
 }
